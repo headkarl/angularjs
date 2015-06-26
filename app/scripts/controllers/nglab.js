f6a8c90bd4063033f21dddd6050f9bf10fbc687f
Wang Zhe <headkarl@163.com>
2015-06-26 20:27:53 +0800

    learning 2015.6.26

--- app/scripts/controllers/nglab.js
+++ app/scripts/controllers/nglab.js
@@ -33,5 +33,40 @@ angular.module('helloworldApp')
     };
     $scope.reset();
 
+
     <!--angularjs测试代码 -->
   });
+
+var ServiceTestModule = angular.module('helloworldApp');
+// var ServiceTestService = angular.module('helloworldApp.services');
+
+ServiceTestModule.factory('Docker', function(docker){
+  var backendUrl = '';
+  var service = {
+    initInfo: [{name:'bc-001',id:'1'},{name:'bc-002',id:'2'},{name:'bc-003',id:'3'}],
+    queryDocker: function(){},
+    setDocker: function(docker){},
+    addDocker: function(docker){},
+    deleteDocker: function(docker){},
+  };
+  return service;
+});
+
+
+
+// .factory('User', function($http) { // injectables go here
+//   var backendUrl = "http://localhost:3000";  var service = {    // our factory definition
+//     user: {},
+//     setName: function(newName) { 
+//       service.user['name'] = newName; 
+//     },
+//     setEmail: function(newEmail) {
+//       service.user['email'] = newEmail;
+//     },
+//     save: function() {
+//       return $http.post(backendUrl + '/users', {
+//         user: service.user
+//       });
+//     }
+//   };  return service;
+// });
